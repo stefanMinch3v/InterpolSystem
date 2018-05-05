44f26763c08c4d918ca402faf56fe9a3a0ea0a9e
Lukas Vaclavek <poortrolls@gmail.com>
2018-05-06 00:32:43 +0200

    Updated pdf certificate content

--- InterpolSystem.Services/ServiceConstants.cs
+++ InterpolSystem.Services/ServiceConstants.cs
@@ -8,13 +8,23 @@
         public const string InvalidFormInfo = "Invalid submitted form.";
 
         public const string PdfCertificateFormat = @"
-<h1>Cerificate</h1>
-<h2>To {0}</h2>
+<h1 style = ""text-align:center""><u>Certificate proving carch of criminal</u></h1>
 <br />
-<h2>Date of issued: {1}</h2>
-<h3>Date of submission {2}</h3>
-<h4>By police department {3}</h4>
-<h5>Wanted person names: {4}</h5>
+<h2 style = ""text-align:center"">To {0}</h2>
+<br />
+<h3 style = ""text-align:center"" >Date of issue: {1}</h2>
+<h3 style = ""text-align:center"" >Date of submission {2}</h3>
+<h4>This certificate proves catch of criminal named :
+            <u>{4}</u> this criminal was escorted to {3}
+        police department. In the name of Interpol we are kindly thanking you
+            for your contribution.
+        </h4>
+<h6 style = ""text-align : right"">
+        Thank you
+        <br> Admiral of Interpoll
+        <br> Tarim Abzhal
+        <br>
+    </h6>
 ";
     }
 }
