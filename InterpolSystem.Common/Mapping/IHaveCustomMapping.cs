fa3df319215e848b4763f61244708917e37ea087
stefan <amxx@abv.bg>
2018-05-25 11:29:31 +0200

    automapper comments

--- InterpolSystem.Common/Mapping/IHaveCustomMapping.cs
+++ InterpolSystem.Common/Mapping/IHaveCustomMapping.cs
@@ -2,6 +2,9 @@
 {
     using AutoMapper;
 
+    /// <summary>
+    /// custom configuration - describes the binding configs for the specific properties
+    /// </summary>
     public interface IHaveCustomMapping
     {
         void ConfigureMapping(Profile mapper);
--- InterpolSystem.Common/Mapping/IMapFrom.cs
+++ InterpolSystem.Common/Mapping/IMapFrom.cs
@@ -1,5 +1,9 @@
 ﻿namespace InterpolSystem.Common.Mapping
 {
+    /// <summary>
+    /// default configuration - binds only the properties with the same names
+    /// </summary>
+    /// <typeparam name="TModel"></typeparam>
     public interface IMapFrom<TModel>
     {
     }
