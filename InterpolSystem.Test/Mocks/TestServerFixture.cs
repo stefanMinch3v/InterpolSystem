527a42ea84975f1d1166e17b438011307f674162
stefan <amxx@abv.bg>
2018-12-06 10:15:24 +0100

    example integration test

---
+++ InterpolSystem.Test/Mocks/TestServerFixture.cs
@@ -0,0 +1,42 @@
+﻿namespace InterpolSystem.Test.Mocks
+{
+    using InterpolSystem.Web;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.TestHost;
+    using Microsoft.Extensions.PlatformAbstractions;
+    using System;
+    using System.IO;
+    using System.Net.Http;
+
+    public class TestServerFixture : IDisposable
+    {
+        // private const string BaseUri = "https://localhost:4368/";
+        private readonly TestServer testServer;
+
+        public TestServerFixture()
+        {
+            var builder = new WebHostBuilder()
+                //.UseContentRoot(GetContentRootPath())
+                .UseStartup<Startup>();
+
+            testServer = new TestServer(builder);
+            Client = testServer.CreateClient();
+            //this.Client.BaseAddress = new Uri(BaseUri);
+        }
+
+        public HttpClient Client { get; }
+
+        public void Dispose()
+        {
+            this.Client?.Dispose();
+            testServer?.Dispose();
+        }
+
+        //private string GetContentRootPath()
+        //{
+        //    var testProjectPath = PlatformServices.Default.Application.ApplicationBasePath;
+        //    var relativePathToHostProject = "../../../../InterpolSystem.Web";
+        //    return Path.Combine(testProjectPath, relativePathToHostProject);
+        //}
+    }
+}
---
+++ InterpolSystem.Test/Web/Controllers/MissingPeopleControllerIntegrationTests.cs
@@ -0,0 +1,26 @@
+﻿namespace InterpolSystem.Test.Web.Controllers
+{
+    using InterpolSystem.Test.Mocks;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class MissingPeopleControllerIntegrationTests : IClassFixture<TestServerFixture>
+    {
+        private readonly TestServerFixture server;
+
+        public MissingPeopleControllerIntegrationTests(TestServerFixture server)
+        {
+            this.server = server;
+        }
+        // TODO SERVER CANNOT FIND FOLDERS
+        //[Fact]
+        //public async Task IndexPageShouldReturnStatusCode200()
+        //{
+        //    var client = this.server.Client;
+        //    var response = await client.GetAsync("/");
+
+        //    response.EnsureSuccessStatusCode();
+        //}
+
+    }
+}
